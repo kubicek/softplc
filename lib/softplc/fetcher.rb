92e61987f9cae560dfc30467820a8ceb2e4d8f73
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2014-12-19 23:12:19 +0100

    Sending request with uuids

--- lib/softplc/fetcher.rb
+++ lib/softplc/fetcher.rb
@@ -16,9 +16,14 @@ module Softplc
       sensor
     end
 
+    def to_request
+      uuids_xml = sensors.collect{|sensor| "<v esgid:vId=\"svc://DefaultConnection/#{sensor.uuid}\" />"}.join
+      '<?xml version="1.0" encoding="utf-8"?><splcComm xmlns="http://dev.rcware.net/xml/esgxmlrequest1.0" xmlns:esgid="http://dev.rcware.net/xml/esgid" xmlns:esg="http://dev.rcware.net/xml/esg" xmlns:esghmi="http://dev.rcware.net/xml/esghmi"><values>' +  uuids_xml + '</values></splcComm>'
+    end
+
     def prefetch
       while sensors.detect{|sensor| sensor.value.nil?}
-        response = RestClient.post "http://#{Softplc.configuration.host}/splchmi.ashx", {}
+        response = RestClient.post "http://#{Softplc.configuration.host}/splchmi.ashx", to_request, {:content_type => :xml}
         doc = Nokogiri.XML(response.body)
         doc.xpath("//xmlns:v[@esgid:vId]").each{|xml|
           uuid = xml.attributes["vId"].value.match(/svc:.*\/(.*)/)[1]
